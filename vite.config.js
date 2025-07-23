4cf30a2d038dde2b0ecfe1899082981a976c0f80
Khawar <hayatkhawar219@gmail.com>
2025-07-23 21:45:31 +0500

    Update code with fix the js conflicts

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'resources/js'),
-    },
-  },
+    plugins: [
+        laravel({
+            input: [
+                'resources/js/app.js',
+                'resources/js/login.js',
+                'resources/css/app.css', // if needed
+            ],
+            refresh: true,
+        }),
+        vue(),
+    ],
+ 
 });
